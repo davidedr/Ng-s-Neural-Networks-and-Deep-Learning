95a9f3a0290ab751273a0923adbe13624c951124
Davide <d.dr@libero.it>
2017-09-02 21:44:14 +0200

    Random image processing added

--- src/image_manipulation/img_manipulation.py
+++ src/image_manipulation/img_manipulation.py
@@ -18,29 +18,38 @@ from libs import utils
 
 files = utils.get_celeb_files() 
 n = 50
+print("Read and plot image n. " + str(n) + ", " + files[n] + "...")
 img = plt.imread(files[n])
 print('image no. ' + str(n) + ', ' + files[n] + ', has shape: ' + str(img.shape) + '.')
 print(img)
 plt.imshow(img)
+plt.title('image no. ' + str(n) + ', ' + files[n])
 plt.show()
 
+print("Plot image n. " + str(n) + ", " + files[n] + " as separate RGB channels...")
 plt.figure()
-plt.subplot(131)
-plt.imshow(img[:, :, 0], cmap = 'gray')
-plt.subplot(132)
-plt.imshow(img[:, :, 1], cmap = 'gray')
-plt.subplot(133)
-plt.imshow(img[:, :, 2], cmap = 'gray')
+plt.subplot(141)
+plt.imshow(img)
+plt.subplot(142)
+plt.imshow(img[:, :, 0], cmap = 'Reds')
+plt.subplot(143)
+plt.imshow(img[:, :, 1], cmap = 'Greens')
+plt.subplot(144)
+plt.imshow(img[:, :, 2], cmap = 'Blues')
 plt.show()
 
+print("Read all images....")
 imgs = utils.get_celeb_imgs()
+n = list(utils.sample_without_replacement(len(files), 1))[0]
 plt.figure()
-plt.imshow(imgs[0])
+plt.imshow(imgs[n])
+plt.title("Example image: " + str(n) + ", " + files[n])
 plt.show()
 
 # Turn into batch array
 data = np.array(imgs)
 
+print("Compute and plot mean and standard deviation images...")
 img_mean = np.mean(data, axis = 0)
 img_stddev = np.std(data, axis = 0)
 
@@ -55,7 +64,8 @@ plt.imshow(img_stddev.astype(np.uint8))
 plt.show()
 
 # Histogram of R, G, B for the n-th image
-n = 1
+print("Compute and plot histograms and estimated densities...")
+n = list(utils.sample_without_replacement(len(files), 1))[0]
 plt.figure()
 plt.subplot(141)
 plt.imshow(imgs[n])
@@ -95,4 +105,26 @@ colours = ('r', 'g', 'b')
 for i, colour in enumerate(colours):
     sns.distplot(img_mean[:, :, i].ravel(), color = colour)
 plt.title("RGB histogram of MEAN image\n(seaborn)")
+plt.show()
+
+# Images normalization
+plt.figure()
+plt.subplot(321)
+plt.imshow(img[n])
+plt.subplot(322)
+sns.distplot(img[n].ravel())
+plt.title('Image'+ str(n) + ", " + files[n])
+
+plt.subplot(323)
+plt.imshow(img_mean.astype(np.uint8))
+plt.subplot(324)
+sns.distplot(img_mean.astype(np.uint8).ravel())
+plt.title('Mean image')
+
+img_nomean = img[n] - img_mean
+plt.subplot(325)
+plt.imshow(img_nomean.astype(np.uint8))
+plt.subplot(326)
+sns.distplot(img_nomean.astype(np.uint8).ravel())
+plt.title('Image  - mean image')
 plt.show()
