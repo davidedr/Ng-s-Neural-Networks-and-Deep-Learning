867f83fe41230fc4561a42fcfb0f6ec4dc148fd8
Davide <d.dr@libero.it>
2017-09-03 20:57:13 +0200

    Convolution with Gaussian kernel added

--- src/image_manipulation/tensorflow_basics.py
+++ src/image_manipulation/tensorflow_basics.py
@@ -64,7 +64,6 @@ s = tf.Session()
 result = z.eval(session = s)
 s.close()
 
-%matplotlib inline
 import matplotlib.pyplot as plt
 plt.plot(result)
 
@@ -79,3 +78,53 @@ s.close()
 
 # A clever way to plot it: see it as an image!
 plt.imshow(result)
+
+##
+## Images convolution with Gaussian kernels
+##
+
+from skimage import data
+img = data.camera().astype(np.float32)
+plt.imshow(img, cmap='gray')
+plt.show()
+print('Image shape: ' + str(img.shape))
+
+image_4d = img.reshape([1, img.shape[0], img.shape[1], 1])
+print('image_4d shape: ' + str(img.shape))
+
+# OR use tf reshape capability:
+img_4d = tf.reshape(img, [1, img.shape[0], img.shape[1], 1])
+print(img_4d)
+print(img_4d.get_shape())
+print(img_4d.get_shape().as_list())
+
+# Reshape the image to N x W x H x C = 1 x W x H x 1 in order to be able to convolve it w/ gaussian kernel
+# Only N = 1 images, only C = 1 channels
+
+# via reshape
+img_4d = img.reshape([1, img.shape[0], img.shape[1], 1])
+print('img_4d.shape (via img.reshape): ' + str(img_4d.shape))
+
+# via tensorflow
+img_4d = tf.reshape(img, [1, img.shape[0], img.shape[1], 1])
+print('img_4d.shape (via tf.reshape): ' + str(img_4d.shape))
+
+# Reshape the kernel to Kernel Height x Kernel Width x Number of Input Channels x Number of Output Channels
+# Number of Input Channels := C = 1
+# Number of Output Channels := Number of Input Channels
+z_4d = tf.reshape(z_2d, [ksize, ksize, 1, 1])
+print('z_4d.shape (via tf.reshape): ' + str(z_4d.shape))
+
+# Perform convolution
+convolved = tf.nn.conv2d(img_4d, z_4d, strides = [1, 1, 1, 1], padding = 'SAME')
+
+s = tf.Session()
+img_convolved = convolved.eval(session = s)
+s.close()
+
+print('img_convolved.shape: ' + str(img_convolved.shape))
+
+# Plot result
+plt.figure()
+plt.imshow(img_convolved[0, :, :, 0], cmap = 'gray')
+plt.show()
